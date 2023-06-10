cd340f113f03f600fcf213c084dbbadbff6d81d3
Yaseer <yaser.hip@gmail.com>
2023-06-09 21:41:11 -0700

    login fixes

--- app/src/main/java/monster/donjabonoso/generalinventory/presentation/login/ScreenLogin.kt
+++ app/src/main/java/monster/donjabonoso/generalinventory/presentation/login/ScreenLogin.kt
@@ -1,15 +1,23 @@
 package monster.donjabonoso.generalinventory.presentation.login
 
+import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -28,6 +36,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -53,48 +62,67 @@ fun ScreenLogin() {
 
     var hidden by remember { mutableStateOf(true) }
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .imePadding()
+            .padding(horizontal = 60.dp, vertical = 10.dp)
+            .verticalScroll(rememberScrollState())
+            .fillMaxSize(), horizontalAlignment = Alignment.End,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(
-            modifier = Modifier.size(60.dp),
-            painter = painterResource(id = R.drawable.inventory_login),
-            contentDescription = null,
-            colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.onSurface)
-        )
-        Text(
-            text = "Inventory", style = TextStyle(
-                fontSize = 40.sp,
-                fontWeight = FontWeight.W800,
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                modifier = Modifier.size(60.dp),
+                painter = painterResource(id = R.drawable.inventory_login),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.onSurface)
             )
-        )
-        Spacer(modifier = Modifier.size(30.dp))
-        OutlinedTextField(value = textEmail,
-            onValueChange = { textEmail = it },
-            label = { Text(text = "Email") })
-        OutlinedTextField(value = textPassword,
-            onValueChange = { textPassword = it },
-            label = { Text(text = "Password") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            visualTransformation = if (hidden) PasswordVisualTransformation() else VisualTransformation.None,
-            trailingIcon = {
-                IconButton(onClick = { hidden = !hidden }) {
-                    val vector = painterResource(
-                        if (hidden) R.drawable.visibility
-                        else R.drawable.visibility_off
-                    )
-                    val description = if (hidden) "Hide password" else "Show password"
-                    Icon(painter = vector, contentDescription = description)
+            Text(
+                text = "Inventory", style = TextStyle(
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.W800,
+                )
+            )
+            Spacer(modifier = Modifier.size(30.dp))
+            OutlinedTextField(value = textEmail,
+                onValueChange = { textEmail = it },
+                singleLine = true,
+                label = { Text(text = "Email") })
+            OutlinedTextField(value = textPassword,
+                onValueChange = { textPassword = it },
+                singleLine = true,
+                label = { Text(text = "Password") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                visualTransformation = if (hidden) PasswordVisualTransformation() else VisualTransformation.None,
+                trailingIcon = {
+                    IconButton(onClick = { hidden = !hidden }) {
+                        val vector = painterResource(
+                            if (hidden) R.drawable.visibility
+                            else R.drawable.visibility_off
+                        )
+                        val description = if (hidden) "Hide password" else "Show password"
+                        Icon(painter = vector, contentDescription = description)
+                    }
+                }
+
+            )
+            Spacer(modifier = Modifier.size(10.dp))
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.TopEnd) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Login")
                 }
             }
 
-        )
-        Spacer(modifier = Modifier.size(15.dp))
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Login")
         }
 
-    }
 
+
+    }
 }
