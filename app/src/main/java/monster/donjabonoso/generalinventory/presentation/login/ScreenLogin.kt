c6edf73738c9470d5588d53d06f71e848a8e7f7c
Yaseer <yaser.hip@gmail.com>
2023-06-24 22:28:16 -0700

    changes

--- app/src/main/java/monster/donjabonoso/generalinventory/presentation/login/ScreenLogin.kt
+++ app/src/main/java/monster/donjabonoso/generalinventory/presentation/login/ScreenLogin.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -27,6 +28,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,11 +37,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -78,6 +84,8 @@ fun ScreenLogin() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            val focusManager = LocalFocusManager.current
+
             Image(
                 modifier = Modifier.size(60.dp),
                 painter = painterResource(id = R.drawable.inventory_login),
@@ -91,15 +99,22 @@ fun ScreenLogin() {
                 )
             )
             Spacer(modifier = Modifier.size(30.dp))
-            OutlinedTextField(value = textEmail,
-                onValueChange = { textEmail = it },
-                singleLine = true,
-                label = { Text(text = "Email") })
-            OutlinedTextField(value = textPassword,
-                onValueChange = { textPassword = it },
-                singleLine = true,
-                label = { Text(text = "Password") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            OutlinedAppTextField(text = textEmail,
+                onChange = {textEmail = it},
+                placeholder = "Email",
+                imeAction = ImeAction.Next,
+                keyboardType = KeyboardType.Email,
+                keyBoardActions = KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    }
+                ))
+            OutlinedAppTextField(
+                text = textPassword,
+                onChange = {textPassword = it},
+                placeholder = "Password",
+                imeAction = ImeAction.Send,
+                keyboardType = KeyboardType.Password,
                 visualTransformation = if (hidden) PasswordVisualTransformation() else VisualTransformation.None,
                 trailingIcon = {
                     IconButton(onClick = { hidden = !hidden }) {
@@ -107,11 +122,9 @@ fun ScreenLogin() {
                             if (hidden) R.drawable.visibility
                             else R.drawable.visibility_off
                         )
-                        val description = if (hidden) "Hide password" else "Show password"
-                        Icon(painter = vector, contentDescription = description)
+                        Icon(painter = vector, contentDescription = "Show/Hide Password")
                     }
                 }
-
             )
             Spacer(modifier = Modifier.size(10.dp))
             Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.TopEnd) {
@@ -121,8 +134,34 @@ fun ScreenLogin() {
             }
 
         }
-
-
-
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OutlinedAppTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    placeholder: String,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    onChange: (String) -> Unit = {},
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    keyBoardActions: KeyboardActions = KeyboardActions(),
+    isEnabled: Boolean = true,
+    singleLine: Boolean = true,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = text,
+        label = { Text(text = placeholder)},
+        onValueChange = onChange,
+        trailingIcon = trailingIcon,
+        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
+        keyboardActions = keyBoardActions,
+        enabled = isEnabled,
+        visualTransformation = visualTransformation,
+        singleLine = singleLine,
+    )
 }
